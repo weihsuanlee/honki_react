b97191f4506b70c4cf482b0eb807bf31a57f6632
Jill Tsai <jilljane32@gmail.com>
2021-02-08 13:47:59 +0800

    更新二手書首頁(連結scss成功) svg圖檔待處理

--- src/jill/pages/UsedBooksIndex.js
+++ src/jill/pages/UsedBooksIndex.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+// 二手書scss
+import '../styles/used-books.scss'
 
 function UsedBooksIndex() {
-  return <h1>測試</h1>
+  return (
+    <>
+      <div className="container my-5">
+        {/* 麵包屑 */}
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-honki">
+              <Link to="#">首頁</Link>
+            </li>
+            <li className="active" aria-current="page">
+              二手交換
+            </li>
+          </ol>
+        </nav>
+        {/* 首頁 */}
+        <h5 className="jill-index-title">選擇模式</h5>
+        <div className="jill-index-bg "></div>
+        <div className="row">
+          <div className="jill-changemode">
+            {/* 一般交換圖片 */}
+
+            {/* 隨機交換圖片 */}
+          </div>
+        </div>
+        <div className="row">
+          <div className="jill-modebtn">
+            <Link to="./normal-index.html">
+              <button className="btn-md-dark jill-normalbtn">一般交換</button>
+            </Link>
+            <Link to="./random-index.html">
+              <button className="btn-md-dark jill-randombtn">隨機交換</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default UsedBooksIndex
