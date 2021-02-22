c5adb73b83d7d444cf188e5777a88eb0d174d94e
jchans <hanschiang86@gmail.com>
2021-02-22 23:54:30 +0800

    不算有進展，單純是上目前的檔案傳到雲端。

--- src/hans/pages/OldSeasons.js
+++ src/hans/pages/OldSeasons.js
@@ -10,7 +10,8 @@ import '../styles/old-seasons.scss'
 
 function OldSeasons() {
   const [targetSolarTerm, setTargetSolarTerm] = useState(0)
-  const [solarTermsListed, setSolarTermsListed] = useState('')
+  const [solarTermToShow, setSolarTermToShow] = useState('')
+  const [solarTermDesc, setSolarTermDesc] = useState('')
 
   // let solarTermList = []
   let currentSolarTerm = 3
@@ -18,19 +19,19 @@ function OldSeasons() {
 
   // 模擬componentDidMount
   useEffect(() => {
-    getDataFromServer()
+    getDataFromServer(currentSolarTerm)
   }, [])
 
   // 和伺服器要資料
-  const getDataFromServer = async () => {
+  const getDataFromServer = async (e) => {
     const response = await fetch('http://localhost:3333/old-seasons', {
       method: 'get',
     })
     const data = await response.json()
     console.log(data)
-    console.log(data['solar_term_list'][1])
-
-    console.log('test', solarTermsListed)
+    console.log(data['solar_term_list'][e])
+    setSolarTermDesc(data['solar_term_list'][e]['st_desc'])
+    setSolarTermToShow(data['solar_term_list'][e]['solar_term'])
 
     // setSolarTermsListed(data)
 
@@ -38,6 +39,7 @@ function OldSeasons() {
     // setTimeout(() => {
     //   console.log('time out')
     // }, 2000)
+    return data
   }
 
   return (
@@ -57,9 +59,11 @@ function OldSeasons() {
               點擊目標節氣書本
             </button>
             <br />
-            要顯示的節氣：{targetSolarTerm}
+            目標節氣書本： {targetSolarTerm}
             <br />
-            {/* 節氣清單：{solarTermsListed[1]['year']} */}
+            節氣：{solarTermToShow}
+            <br />
+            描述：{solarTermDesc}
           </div>
 
           <div className="col-xl-6 osb-book-col-outer">
