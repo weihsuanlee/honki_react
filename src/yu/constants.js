dc14b19c4fa4ca0bac0250e3f945b94859a4b5ac
biancachien <cocoamacaron@gmail.com>
2021-02-22 11:15:53 +0800

    傳節氣頭貼圖檔

--- src/yu/constants.js
+++ src/yu/constants.js
@@ -8,3 +8,64 @@ export const SVG_FACES = [
   '/images/yu/peepsphotos/face/Smile Teeth Gap.svg',
   '/images/yu/peepsphotos/face/Awe.svg',
 ]
+
+export const SVG_HAIR = [
+  '/images/yu/peepsphotos/hair/1Bangs 2.svg',
+  '/images/yu/peepsphotos/hair/2Bun 2.svg',
+  '/images/yu/peepsphotos/hair/3Long Bangs.svg',
+  '/images/yu/peepsphotos/hair/4Medium Bangs 3.svg',
+  '/images/yu/peepsphotos/hair/5hat-hip.svg',
+  '/images/yu/peepsphotos/hair/6Pomp.svg',
+  '/images/yu/peepsphotos/hair/7Shaved 2.svg',
+  '/images/yu/peepsphotos/hair/8Twists 2.svg',
+]
+
+export const SVG_CLOTH = [
+  '/images/yu/peepsphotos/cloth/Button Shirt 1.svg',
+  '/images/yu/peepsphotos/cloth/Device.svg',
+  '/images/yu/peepsphotos/cloth/Dress.svg',
+  '/images/yu/peepsphotos/cloth/Fur Jacket.svg',
+  '/images/yu/peepsphotos/cloth/Hoodie.svg',
+  '/images/yu/peepsphotos/cloth/Polo and Sweater.svg',
+  '/images/yu/peepsphotos/cloth/Striped Tee.svg',
+  '/images/yu/peepsphotos/cloth/Tee 1.svg',
+]
+
+export const SVG_ACC = [
+  '/images/yu/peepsphotos/acc/_ None.svg',
+  '/images/yu/peepsphotos/acc/Glasses 2.svg',
+  '/images/yu/peepsphotos/acc/Glasses 3svg',
+  '/images/yu/peepsphotos/acc/Glasses 4.svg',
+  '/images/yu/peepsphotos/acc/Glasses 5.svg',
+  '/images/yu/peepsphotos/acc/Glasses.svg',
+  '/images/yu/peepsphotos/acc/Sunglasses 2.svg',
+  '/images/yu/peepsphotos/acc/Sunglasses.svg',
+]
+
+export const SVG_TERMS = [
+  '/images/yu/peepsphotos/24trems/trem1.svg',
+  '/images/yu/peepsphotos/24trems/trem2.svg',
+  '/images/yu/peepsphotos/24trems/trem3.svg',
+  '/images/yu/peepsphotos/24trems/trem4.svg',
+  '/images/yu/peepsphotos/24trems/trem5.svg',
+  '/images/yu/peepsphotos/24trems/trem6.svg',
+  '/images/yu/peepsphotos/24trems/trem7.svg',
+  '/images/yu/peepsphotos/24trems/trem8.svg',
+  '/images/yu/peepsphotos/24trems/trem9.svg',
+  '/images/yu/peepsphotos/24trems/trem10.svg',
+  '/images/yu/peepsphotos/24trems/trem11.svg',
+  '/images/yu/peepsphotos/24trems/trem12.svg',
+  '/images/yu/peepsphotos/24trems/trem13.svg',
+  '/images/yu/peepsphotos/24trems/trem14.svg',
+  '/images/yu/peepsphotos/24trems/trem15.svg',
+  '/images/yu/peepsphotos/24trems/trem16.svg',
+  '/images/yu/peepsphotos/24trems/trem17.svg',
+  '/images/yu/peepsphotos/24trems/trem18.svg',
+  '/images/yu/peepsphotos/24trems/trem19.svg',
+  '/images/yu/peepsphotos/24trems/trem20.svg',
+  '/images/yu/peepsphotos/24trems/trem21.svg',
+  '/images/yu/peepsphotos/24trems/trem22.svg',
+  '/images/yu/peepsphotos/24trems/trem23.svg',
+  '/images/yu/peepsphotos/24trems/trem24.svg',
+  
+]
