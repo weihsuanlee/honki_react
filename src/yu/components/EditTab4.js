6824fb90512f3684c6c27c365c1a07341ef916af
biancachien <cocoamacaron@gmail.com>
2021-02-24 10:22:56 +0800

    修改刪除帳號頁面

--- src/yu/components/EditTab4.js
+++ src/yu/components/EditTab4.js
@@ -1,10 +1,35 @@
-import { Link } from 'react-router-dom'
+import { useRef } from 'react'
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
 
 function EditTab4() {
+  const divRef = useRef()
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [passwordConfirm2, setPasswordConfirm2] = useState('')
+  const sid = localStorage.getItem('userId')
+
+  const passwordForDelete = async function (passwordConfirm, sid) {
+    const url = 'http://localhost:3333/member/editdeleteaccount'
+    const request = new Request(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        password: passwordConfirm,
+        sid: sid,
+      }),
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
+    const response = await fetch(request)
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <>
       {/* <!-- 收合選單-4刪除帳號 --> */}
-      <div className="panel" id="four-panel">
+      <div ref={divRef} className="panel" id="four-panel">
         <div className="panel-title yu-edit-delete-sentence">
           真的要離開我們嗎......
         </div>
@@ -33,6 +58,11 @@ function EditTab4() {
               className="form-control formInput col-6"
               id="yu-edit-delete-password"
               name="password"
+              required
+              value={passwordConfirm}
+              onChange={(e) => {
+                setPasswordConfirm(e.target.value)
+              }}
             />
           </div>
         </div>
@@ -48,8 +78,13 @@ function EditTab4() {
             <input
               type="password"
               className="form-control formInput col-6"
-              id="yu-edit-delete-password-comfirm"
+              id="yu-edit-delete-password-confirm"
               name="password"
+              required
+              value={passwordConfirm2}
+              onChange={(e) => {
+                setPasswordConfirm2(e.target.value)
+              }}
             />
           </div>
         </div>
@@ -57,14 +92,14 @@ function EditTab4() {
         {/* <!-- 按鈕 --> */}
         <div className="form-group button-group">
           <div className="formItems row d-flex justify-content-center">
-            <Link to="/">
-              <button
-                type="button"
-                className="btn-md-dark form-button form-control yu-edit-delete-button"
-              >
-                確定刪除
-              </button>
-            </Link>
+            <Button
+              className="btn-md-dark form-button form-control yu-edit-delete-button col-3"
+              onClick={() => {
+                passwordForDelete(passwordConfirm, sid)
+              }}
+            >
+              確定刪除
+            </Button>
           </div>
         </div>
       </div>
