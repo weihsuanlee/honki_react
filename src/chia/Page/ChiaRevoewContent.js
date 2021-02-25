3369309cf264a7516a36d8dcaf372033417f1053
Adriani Kao <adrianikao119@gmail.com>
2021-02-25 20:51:37 +0800

    小修:D

--- src/chia/Page/ChiaRevoewContent.js
+++ src/chia/Page/ChiaRevoewContent.js
@@ -108,6 +108,9 @@ function ChiaReviewContent(props) {
                   {ContentTop}
                   {Content}
                 </div>
+                <div className="chia_thumbup">
+                  <FaThumbsUp />
+                </div>
               </div>
             </div>
           </div>
