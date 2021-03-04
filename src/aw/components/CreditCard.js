5b2374eca60d0247427cbaaea34ab47d2c120d48
ArthurWu <arthurwu315@hotmail.com>
2021-03-04 17:09:32 +0800

    信用卡樣式更改

--- src/aw/components/CreditCard.js
+++ src/aw/components/CreditCard.js
@@ -23,7 +23,7 @@ export default class CreditCard extends React.Component {
 
   render() {
     return (
-      <div id="PaymentForm">
+      <div id="PaymentForm" className="aw-productAreaPadding mb-5 mt-5">
         <Cards
           cvc={this.state.cvc}
           expiry={this.state.expiry}
@@ -31,43 +31,69 @@ export default class CreditCard extends React.Component {
           name={this.state.name}
           number={this.state.number}
         />
-        <form>
-          <input
-            type="tel"
-            name="number"
-            className="form-control formInput"
-            placeholder="Card Number"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-            oninput="value=value.replace(/[^\d]/g,'')"
-            maxlength="16"
-          />
-          <input
-            type="tel"
-            name="name"
-            className="form-control formInput"
-            placeholder="Card name"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-            oninput="value=value.replace( /[^\a-\z\A-\Z]/g,'')"
-            maxlength="16"
-          />
-          <input
-            type="tel"
-            name="expiry"
-            className="form-control formInput"
-            placeholder="Card expiry"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
-          <input
-            type="tel"
-            name="cvc"
-            className="form-control formInput"
-            placeholder="Card cvc"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
+        <div className="mt-5 mb-2">
+          <h6>請填寫信用卡資訊</h6>
+        </div>
+        <form className="mt-4">
+          <div class="form-group ">
+            <div class="formItems row d-flex">
+              <label class="inputText col-5">卡號</label>
+              <input
+                type="tel"
+                name="number"
+                className="form-control formInput col-7"
+                placeholder="Card Number"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                oninput="value=value.replace(/[^\d]/g,'')"
+                maxlength="16"
+              />
+            </div>
+          </div>
+
+          <div class="form-group ">
+            <div class="formItems row d-flex">
+              <label class="inputText col-5">持卡人</label>
+              <input
+                type="tel"
+                name="name"
+                className="form-control formInput col-7"
+                placeholder="Card name"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                oninput="value=value.replace( /[^\a-\z\A-\Z]/g,'')"
+                maxlength="16"
+              />{' '}
+            </div>
+          </div>
+
+          <div class="form-group ">
+            <div class="formItems row d-flex">
+              <label class="inputText col-5">到期日</label>
+              <input
+                type="tel"
+                name="expiry"
+                className="form-control formInput col-7"
+                placeholder="Card expiry"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />{' '}
+            </div>
+          </div>
+
+          <div class="form-group ">
+            <div class="formItems row d-flex">
+              <label class="inputText col-5">驗證碼</label>
+              <input
+                type="tel"
+                name="cvc"
+                className="form-control formInput col-7"
+                placeholder="Card cvc"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />{' '}
+            </div>
+          </div>
         </form>
       </div>
     )
